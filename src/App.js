1b8b2cd8a0a1aaa3dd8718ff2a1839bc5848f3cc
Musab Akan <104889820+MusabAkan@users.noreply.github.com>
2023-09-15 00:16:58 +0300

    #github search all

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
- function App() {
-  return (
-    <div className="bg-primary">
-     <h1>github finder 2</h1> 
+import Navbar from './components/Navbar';
+import UserList from './components/UserList';
+import React, { Component } from 'react'
+import Search from './components/Search';
+
+export class App extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      loading: false,
+      users: []
+    }
+  }
+
+  searchUsers = (keyword) => {
+
+    this.setState({ loading: true });
+
+    setTimeout(() => {
+      fetch("https://api.github.com/search/users?q=" + keyword)
+        .then(response => response.json())
+        .then(data => this.setState({ users: data.items, loading: false }));
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Search searchUsers={this.searchUsers}/>
+        <div className="container mt-3">
+          <UserList users={ this.state.users } loading={this.state.loading}/>
+        </div>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
